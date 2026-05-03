5fec9781e5c8569952c4ea7e6e9875f77c564bbe
stephanie nyangate <stephanienyangate@gmail.com>
2026-05-04 00:00:31 +0300

    correct errors

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
- function findAkanName() {
-   // This alert button is officially working alert("calculating your Akan Name...");
-let birthday=document.getElementById("birthday");
-let genderElement=document.querySelector('input[name="gender"]:checked');
-  
-   // check if the user filled the form
-if(!birthday.value ||!genderElement){ 
-    alert("Please select both a date and a gender!"); 
-    return;
+function findAkanName() {
+    // This alert will tell us the button is officially working
+   alert("calculating your Akan Name ...");
+
+    let birthday=document.getElementById("birthday");
+    let genderElement=document.querySelector('input[name="gender"]:checked');
+
+    //  Check if the user actually filled in the form
+    if (!birthday.value || !genderElement) {
+        alert("Please select both a date and a gender!");
+        return;
+    }
+
+    let gender =genderElement.value;
+    let dateParts = birthday.value.split('-');
+
+    // Extract Year, Month, and Day (using numbers)
+    let year = parseInt(dateParts[0]);
+    let month = parseInt(dateParts[1]);
+    let day = parseInt(dateParts[2]);
+
+    // Formula Variables
+    let CC = parseInt(year.toString().slice(0, 2));
+    let YY = parseInt(year.toString().slice(2, 4));
+    let MM = month;
+    let DD = day;
+
+    // The Formula 
+    let dayOfWeek = Math.floor((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
+
+    // Akan Name Lists
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    // Picking the name
+    let akanName = "";
+    if (gender === "male") {
+        akanName = maleNames[dayOfWeek];
+    } else {
+        akanName = femaleNames[dayOfWeek];
+    }
+
+    // Displaying the Result on the screen
+    document.getElementById("result").innerHTML = "Your Akan Name is <strong>" + akanName + "</strong>!";
 }
-let gender=genderElement.value;
-let dateParts=birthday.value.split('-');
-    
-   //  Extract Year,Month,and Day(using numbers)
-let year= parseInt(dateParts[0]);
-let month =parseInt(dateParts[1]);
-let day =parseInt(dateParts[2]);
-
-   // Formula Variables
-let CC =parseInt(year.toString().slice(0,2));
-let YY =parseInt(year.toString().slice(2,4));
-let MM =month;
-let DD =day;
-  
-   //The formula
-let dayOfWeek =Math.floor((((CC/4)-2*CC- 1) + ((5* YY/4))+ ((26*(MM+1) /10)) + DD) % 7);
-
-   // Akan Name List
-let maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleNames =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-   // picking the names
-let akanName ="";
-if (gender==="male") {akanName=maleNames[dayOfWeek];} else{akanName = femaleNames[dayOfWeek];}
-
-   // Displaying the results
-document.getElementById("result").innerHTML ="Your Akan Name is <strong>"+ akanName + </strong>!;
- }
-
